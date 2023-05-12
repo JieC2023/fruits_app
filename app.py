b4e32f35415a17425c4e8a8e5e1e9619f7b13799
jie_chen <jie.chen@outlook.com.au>
2023-05-12 15:27:41 +0930

    add edit function

--- app.py
+++ app.py
@@ -31,3 +31,20 @@ def create():
   carbohydrates = request.form.get('carbohydrates')
   sql('INSERT INTO fruits(name, image_url, sugar, calories, carbohydrates) VALUES(%s, %s, %s, %s, %s) RETURNING *', [name, image_url, sugar, calories, carbohydrates])
   return redirect('/')
+
+@app.route('/fruits/<id>/edit')
+def edit(id):
+  fruits = sql("SELECT * FROM fruits WHERE id = %s", [id])
+  fruit = fruits[0]
+  return render_template('fruits/edit.html', fruit=fruit)
+
+@app.route('/fruits/<id>', methods=["POST"])
+def update(id):
+  name = request.form.get('name')
+  image_url = request.form.get('image_url')
+  sugar = request.form.get('sugar')
+  calories = request.form.get('calories')
+  carbohydrates = request.form.get('carbohydrates')
+
+  sql('UPDATE fruits SET name=%s, image_url=%s, sugar=%s, calories=%s, carbohydrates=%s WHERE id=%s RETURNING *', [name, image_url, sugar, calories, carbohydrates, id])
+  return redirect('/')
